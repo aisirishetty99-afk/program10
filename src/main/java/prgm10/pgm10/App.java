d90706735e0b9d980344317d2a6a738ea8f670fa
aisirishetty99-afk <aisirishetty99@gmail.com>
2026-04-23 08:34:16 +0530

    first commit

---
+++ src/main/java/prgm10/pgm10/App.java
@@ -0,0 +1,13 @@
+package prgm10.pgm10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
